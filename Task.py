bbd9ac64ce097499bc3c8fdfa310608c4ab42aa1
Roderic <rodericgc@gmail.com>
2020-04-07 12:07:36 -0400

    enhanced isSubshape

--- Task.py
+++ Task.py
@@ -213,18 +213,28 @@ class Shape:
         return True
     
     #for now ignore rotation or symmetry
-    def isSubshape(self, other, sameColor=False):
+    def isSubshape(self, other, sameColor=False, rotation=False):
         if sameColor:
             if self.color != other.color:
                 return False
-            
-        if self.xLen > other.xLen or self.yLen > other.yLen:
-            return False
-        
-        for po in other.pixels:
-            if set([tuple(np.add(ps,po)) for ps in self.pixels]) <= other.pixels:
-                return True
+        for yTr in range(other.yLen - self.yLen + 1):
+            for xTr in range(other.xLen - self.xLen + 1):
+                if set([tuple(np.add(ps,[xTr,yTr])) for ps in self.pixels]) <= other.pixels:
+                    return True
+        if rotation:
+            #maybe compute shapeDummy just once
+            for x in range(1,4):
+                if Shape(np.array(np.rot90(self.shapeDummyMatrix(),x).nonzero()).transpose(),self.color,self.isBorder).isSubshape(other):
+                    return True
         return False
+    
+    
+        #if self.xLen > other.xLen or self.yLen > other.yLen:
+        #    return False
+        #for po in other.pixels:
+        #    if set([tuple(np.add(ps,po)) for ps in self.pixels]) <= other.pixels:
+        #        return True
+        #return False
     
     
     def isLRSymmetric(self):
